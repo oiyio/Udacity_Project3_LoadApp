2359f8b3cca17cdd98583614c4e147fa4b74e886
OMERIYIOZ <omer.iyioz@turkcell.com.tr>
2021-02-12 12:25:09 +0300

    button background color change wrt progress animation done.

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -6,7 +6,9 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Rect
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
@@ -22,19 +24,33 @@ class LoadingButton @JvmOverloads constructor(
 
     private var rectText = Rect()
 
-    private val valueAnimator = ValueAnimator()
+    private var valueAnimator = ValueAnimator()
+
+    private var progressWidth = 0
 
     private var paintBackground = Paint(Paint.ANTI_ALIAS_FLAG)
     private var paintText = Paint(Paint.ANTI_ALIAS_FLAG)
 
     var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
-        when(new){
-            ButtonState.Loading ->{
+        when (new) {
+            ButtonState.Loading -> {
+
+                valueAnimator = ValueAnimator.ofInt(0, widthSize).apply {
+                    duration = 2500
+                    addUpdateListener { valueAnimator ->
+                        progressWidth = animatedValue as Int
+                        invalidate()
+                    }
+                    start()
+                }
+
                 buttonText = context.getString(R.string.button_loading)
                 invalidate()
             }
-            ButtonState.Completed ->{
+            ButtonState.Completed -> {
+                valueAnimator.end()
                 buttonText = context.getString(R.string.download)
+                progressWidth = 0
                 invalidate()
             }
         }
@@ -64,10 +80,24 @@ class LoadingButton @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        paintBackground.color = ContextCompat.getColor(context, R.color.colorPrimary)
         canvas.drawRect(0f, 0f, widthSize.toFloat(), heightSize.toFloat(), paintBackground)
 
+        paintBackground.color = ContextCompat.getColor(context, R.color.colorPrimaryDark)
+        canvas.drawRect(
+            0f,
+            0f,
+            widthSize.toFloat() * progressWidth / 100,
+            heightSize.toFloat(), paintBackground
+        )
+
         paintText.getTextBounds(buttonText, 0, buttonText.length, rectText)
-        canvas.drawText(buttonText, width/2f-rectText.centerX(), height/2f - rectText.centerY(), paintText)
+        canvas.drawText(
+            buttonText,
+            width / 2f - rectText.centerX(),
+            height / 2f - rectText.centerY(),
+            paintText
+        )
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
